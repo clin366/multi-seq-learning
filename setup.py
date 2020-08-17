deb20e11819af10350c68eae6243ed2128077984
linchen <clin366@gatech.edu>
2020-08-16 22:51:26 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Multivarite time series prediction with missing values',
+    author='Emory University',
+    license='MIT',
+)
